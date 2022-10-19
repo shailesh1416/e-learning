ade1b3caa8a96a5d1cd5052aeb8a8c1f7e535022
shaileshyadav1416 <shaileshyadav1416@gmail.com>
2022-10-19 10:14:53 +0530

    database settings updated

--- academy/settings.py
+++ academy/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'academy.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-   
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'shaileshyadav141$elearning',
-    #     'USER': 'shaileshyadav141',
-    #     'PASSWORD': 'Database@007',
-    #     'HOST': 'shaileshyadav1416.mysql.pythonanywhere-services.com',
-
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+   
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shaileshyadav141$elearning',
+        'USER': 'shaileshyadav141',
+        'PASSWORD': 'Database@007',
+        'HOST': 'shaileshyadav1416.mysql.pythonanywhere-services.com',
+
+    }
 
 }
 
